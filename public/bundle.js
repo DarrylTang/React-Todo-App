7054d97cbdaf5da6cf0b423dbd2241ed56962f2f
Darryl <darryltanggy@gmail.com>
2018-05-15 12:09:04 +0800

    uppercase search fix

--- public/bundle.js
+++ public/bundle.js
@@ -43906,7 +43906,7 @@
 	        // filter by searchText
 	        filteredTodos = filteredTodos.filter(function (todo) {
 	            var text = todo.text.toLowerCase();
-	            return searchText.length === 0 || text.indexOf(searchText) > -1;
+	            return searchText.length === 0 || text.indexOf(searchText.toLowerCase()) > -1;
 	        });
 
 	        // sort todos with non-completed first
